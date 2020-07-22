31a5c38f8bc679ebf442f226347e8ae81143eb10
DOMINGO LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <LIANGDO@OOCL.COM>
2020-07-22 17:37:53 +0800

    [OO Step by Step] [Domingo Liang] feature : upload practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,19 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+
+    private int Class;
+
+    public Student(String name, int age, int Class) {
+        super(name, age);
+        this.Class = Class;
+    }
+
+    public String introduce() {
+        return ("I am a Student. I am at Class " + Class + ".");
+    }
+
+    public int getKlass() {
+        return Class;
+    }
 }
