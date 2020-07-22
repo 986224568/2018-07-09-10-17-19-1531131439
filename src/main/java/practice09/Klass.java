46e00a5bd64e29c0b65b00ff3daeee9bbc4c39e3
986224568 <986224568@qq.com>
2020-07-22 22:42:43 +0800

    [OO Step by Step] [Domingo Liang] feature : upload practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,34 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (!this.equals(student.getKlass())) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        this.leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
 }
