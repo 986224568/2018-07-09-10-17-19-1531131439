7eac10b6730a2824d0aba7ccdd79f0c742356c2b
DOMINGO LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <LIANGDO@OOCL.COM>
2020-07-22 17:40:36 +0800

    [OO Step by Step] [Domingo Liang] feature : upload practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,19 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+
+    private int Class;
+
+    public Student(String name, int age, int Class) {
+        super(name, age);
+        this.Class = Class;
+    }
+
+    public String introduce() {
+        return ("My name is "+name+". I am "+age+" years old. I am a Student. I am at Class "+Class+".");
+    }
+
+    public int getKlass() {
+        return Class;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = 0;
+    }
+
+    public String introduce() {
+        if (klass == 0) {
+            return ("My name is "+name+". I am "+age+" years old. I am a Teacher. I teach No Class.");
+        }
+        return ("My name is "+name+". I am "+age+" years old. I am a Teacher. I teach Class "+klass+".");
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
