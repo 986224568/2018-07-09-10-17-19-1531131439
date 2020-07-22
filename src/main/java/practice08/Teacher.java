f7b3ee4663e5048917be0067b114607e5bd8c1e3
986224568 <986224568@qq.com>
2020-07-22 22:41:58 +0800

    [OO Step by Step] [Domingo Liang] feature : upload practice08

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,33 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+
+    public String introduce() {
+        if (klass == null) {
+            return (super.introduce() + " I am a Teacher. I teach No Class.");
+        }
+        return (super.introduce() + " I am a Teacher. I teach Class "+ klass.getNumber() +".");
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == this.klass.getNumber()) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }
