2707317a73b362b840161b428add8a8de4fc38cc
DOMINGO LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <LIANGDO@OOCL.COM>
2020-07-22 17:39:49 +0800

    [OO Step by Step] [Domingo Liang] feature : upload practice04

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,23 @@
 package practice04;
 
 public class Person {
+    protected String name;
+    protected int age;
+
+    public String introduce() {
+        return ("My name is "+ name +". I am "+ age +" years old.");
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
